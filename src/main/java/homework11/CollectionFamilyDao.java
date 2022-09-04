e57d5ccd84e04fdec79329fa43edc847dc854018
ahajiyev628 <66174474+ahajiyev628@users.noreply.github.com>
2022-09-04 15:35:24 +0400

    Update CollectionFamilyDao.java

--- src/main/java/homework11/CollectionFamilyDao.java
+++ src/main/java/homework11/CollectionFamilyDao.java
@@ -2,6 +2,7 @@ package homework11;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CollectionFamilyDao implements FamilyDao {
     List<Family> families;
@@ -62,9 +63,106 @@ public class CollectionFamilyDao implements FamilyDao {
         return families;
     }
 
+    @Override
+    public List<Family> displayAllFamilies() {
+        return families.stream().toList();
+    }
+
+    @Override
+    public List<Family> getFamiliesBiggerThan(int n) {
+        return families
+                .stream()
+                .filter(f->f.countFamily()>n)
+                .collect(Collectors.toList());
+    }
+    @Override
+    public List<Family> getFamiliesLessThan(int n) {
+        return families
+                .stream()
+                .filter(f->f.countFamily()<n)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public int countFamiliesWithMemberNumber() {
+        List<Family> fam = families
+                .stream()
+                .filter(f->f.countFamily()==4)
+                .collect(Collectors.toList());
+        return fam.size();
+    }
+
+    @Override
+    public void createNewFamily(Human father, Human mother, List<Human> children) {
+        families.add(new Family(father,mother, children));
+        System.out.println("new Family is created and added to database");
+    }
+
+    @Override
+    public void deleteFamilyByIndex(int index) {
+        deleteFamily(index);
+        System.out.println(index + "th family was removed from DAO");
+    }
+
+    @Override
+    public Family deleteAllChildrenOlderThen(Family f, int n) {
+        List<Human> childrens = f.getChildren()
+                .stream()
+                .filter(a->a.getAge()<n)
+                .collect(Collectors.toList());
+
+        f.setChildren(childrens);
+        return f;
+    }
+
+    @Override
+    public void bornChild(Family family, String gender) {
+        if(gender.equals("masculine")){
+            families
+                    .stream().filter(f->f.equals(family))
+                    .findFirst()
+                    .get()
+                    .addChild(new Human("masculine name","masculine surname"));
+
+        }else if (gender.equals("feminine")) {
+            families
+                    .stream().filter(f->f.equals(family))
+                    .findFirst().get()
+                    .addChild(new Human("feminine name","feminine surname"));
+        }
+    }
+
+    @Override
+    public Family adoptChild(Family f, Human child) {
+        f.addChild(child);
+        return f;
+    }
+
+    @Override
+    public int count() {
+        return getAllFamilies().size();
+    }
+
+    @Override
+    public Family getFamilyById(int id) {
+        return getAllFamilies().get(id);
+    }
+
+    @Override
+    public List<Pet> getPets(int index) {
+        return getFamilyById(index).getPet().stream().collect(Collectors.toList());
+    }
+
+    @Override
+    public void addPet(int index, Pet p) {
+        getFamilyById(index).getPet().add(p);
+        System.out.println("a new pet is added to the specified family");
+    }
+
     public void create(Family family) {
         getAllFamilies().add(family);
         System.out.println("new family is added to DAO");
     }
+
 }
 
