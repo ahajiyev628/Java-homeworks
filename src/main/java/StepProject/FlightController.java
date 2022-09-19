8aa3fbfd074b9cba5038992531051dc4599844af
Allahverdi Hajiyev <ahajiyev628@gmail.com>
2022-09-19 20:46:07 +0400

    first commit

--- src/main/java/StepProject/FlightController.java
+++ src/main/java/StepProject/FlightController.java
@@ -6,4 +6,5 @@ public class FlightController {
     public FlightController(FlightService fs) {
         this.fs = fs;
     }
+
 }
--- src/main/java/StepProject/Main.java
+++ src/main/java/StepProject/Main.java
@@ -5,21 +5,20 @@ import java.text.ParseException;
 import java.util.*;
 
 public class Main {
-    public static String path = "/home/ufaz/Downloads/test3.txt";
+    public static String flightPath = "/home/ufaz/Downloads/test3.txt";
+    public static String bookingPath = "/home/ufaz/Downloads/test4.txt";
 
     public static void main(String[] args) throws ParseException, IOException {
         Scanner sc = new Scanner(System.in);
 
-        File file = new File(path);
+        File flightFile = new File(flightPath);
+        File bookingFile = new File(bookingPath);
 
-        FlightService fs =  new FlightService(new CollectionFlightDao(file));
+        FlightService fs =  new FlightService(new CollectionFlightDao(flightFile));
+        BookingService bs =  new BookingService(new CollectionBookingDao(bookingFile));
 
         FlightController fc = new FlightController(fs);
-
-        fc.fs.cfd.getAllFlight();
-
-        // fc.fs.cfd.saveFlight(new Flight(12,"Baku","Kiev","24/11/1998","10:24",10));
-        //fc.fs.cfd.saveFlight(new Flight("Kiev","Baku"));
+        BookingController bc = new BookingController(bs);
 
         fc.fs.generate();
 
@@ -55,6 +54,8 @@ public class Main {
         }
 
         // System.out.println(fc.fs.searchFlight("Baku","2023/03/02",5));
+
+       // bc.bs.cbd.
 
     }
 }
