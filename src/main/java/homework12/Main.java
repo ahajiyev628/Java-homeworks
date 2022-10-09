091f5bfc9903804443beded674a63f372963984b
Allahverdi Hajiyev <ahajiyev628@gmail.com>
2022-10-09 18:59:09 +0400

    first commit

--- src/main/java/homework12/Main.java
+++ src/main/java/homework12/Main.java
@@ -21,8 +21,7 @@ public class Main {
 
         FamilyService fs = new FamilyService(new CollectionFamilyDao());
 
-        fs.createNewFamily(father2,mother2);
-
+        fs.createNewFamily(father2, mother2);
 
 
         //System.out.println("mother: " + mother2.toString());
@@ -33,7 +32,7 @@ public class Main {
         Set<String> petHabits = new HashSet<String>();
         Set<String> catHabits = new HashSet<String>();
 
-        Pet dog = new Pet(AnimalSpecies.DOG,"wimel", 6, 9, (HashSet<String>) petHabits) {
+        Pet dog = new Pet(AnimalSpecies.DOG, "wimel", 6, 9, (HashSet<String>) petHabits) {
             @Override
             public String eat() {
                 return null;
@@ -115,16 +114,16 @@ public class Main {
 
         ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        fs.createNewFamily(new Human(),new Human());
+        fs.createNewFamily(new Human(), new Human());
         Family family2 = fs.getAllFamilies().get(1);
         family2.setMother(new Human());
         family2.setFather(new Human());
         family2.setChildren(new ArrayList<>());
         family2.setPet(new HashSet<>());
 
-        fs.bornChild(family2,new Human(),gender_boy);
-        fs.bornChild(family2,new Human(),gender_girl);
-        fs.bornChild(family2,new Human(),gender_boy);
+        fs.bornChild(family2, new Human(), gender_boy);
+        fs.bornChild(family2, new Human(), gender_girl);
+        fs.bornChild(family2, new Human(), gender_boy);
 
         family2.getChildren().get(0).setSchedule(new HashMap<>());
         family2.getChildren().get(1).setSchedule(new HashMap<>());
@@ -137,110 +136,132 @@ public class Main {
         //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
         Iterator<Family> it = fs.getAllFamilies().iterator();
-        for(int i=1; it.hasNext(); i++) {
+        for (int i = 1; it.hasNext(); i++) {
             System.out.println(i + " Family:    " + it.next());
         }
 
         //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        System.out.println();
+        System.out.println();
         Scanner sc = new Scanner(System.in);
 
-        try {
-            System.out.print("Enter a number to get families having number of members bigger than that number: ");
-            Collection<Family> f1 = fs.getFamiliesBiggerThan(sc.nextInt());
-            Iterator<Family> iter1 = f1.iterator();
-            for(int i=1; iter1.hasNext(); i++) {
-                System.out.println(i + " Family:    " + iter1.next());
+
+        System.out.println();
+        System.out.println("Type '1' if you are looking for families with the most family members");
+        System.out.println("Type '2' if you are looking for families with the least family members");
+        System.out.println("Type '3' if you are looking for families with the specified family members");
+        System.out.println("Type '4' if you want to create a new family");
+        System.out.println("Type '5' if you want to delete a family");
+        System.out.println("Type '6' if you want to edit a family member");
+        System.out.println("Type '7' if you want to adopt a new child for the family");
+        System.out.println();
+
+        boolean forAllMethods = true;
+        while (forAllMethods) {
+            System.out.println("Now, enter one of the 6 number or type 'exit' to exit: ");
+            String navBar = sc.nextLine();
+            if (navBar.equals("1")) {
+                try {
+                    System.out.print("Enter a number to get families having number of members bigger than that number: ");
+                    Collection<Family> f1 = fs.getFamiliesBiggerThan(sc.nextInt());
+                    Iterator<Family> iter1 = f1.iterator();
+                    for (int i = 1; iter1.hasNext(); i++) {
+                        System.out.println(i + " Family:    " + iter1.next());
+                    }
+                } catch (Exception ex) {
+                    System.err.println("No familes found");
+                }
+            } else if (navBar.equals("2")) {
+                try {
+                    System.out.print("Enter a number to get families having number of members less than that number: ");
+                    Collection<Family> f2 = fs.getFamiliesLessThan(sc.nextInt());
+                    Iterator<Family> iter2 = f2.iterator();
+                    for (int i = 1; iter2.hasNext(); i++) {
+                        System.out.println(i + " Family:    " + iter2.next());
+                    }
+                } catch (Exception ex) {
+                    System.err.println("No familes found");
+                }
+            } else if (navBar.equals("3")) {
+                System.out.print("Enter a number to get families having number of members equals that number: ");
+                int member_cnt = sc.nextInt();
+                List<Family> fmm = fs.getAllFamilies().stream()
+                        .filter(s -> s.getChildren().size() + 2 == member_cnt)
+                        .collect(Collectors.toList());
+                Iterator<Family> iter3 = fmm.iterator();
+                for (int i = 1; iter3.hasNext(); i++) {
+                    System.out.println(i + " Family:    " + iter3.next());
+                }
+            } else if (navBar.equals("4")) {
+                Human father3 = new Human();
+                System.out.print("father name: ");
+                father3.setName(sc.nextLine());
+                System.out.print("father surname: ");
+                father3.setSurname(sc.nextLine());
+                System.out.print("father IQ: ");
+                father3.setIq(sc.nextInt());
+                father3.setBirthDate(sc.nextLine());
+                father3.setSchedule(new HashMap<>());
+                System.out.print("father task: ");
+                father3.getSchedule().put(DayOfWeek.Thursday, sc.nextLine());
+
+                Human mother3 = new Human();
+                System.out.print("mother name: ");
+                mother3.setName(sc.nextLine());
+                System.out.print("mother surname: ");
+                mother3.setSurname(sc.nextLine());
+                System.out.print("mother IQ: ");
+                mother3.setIq(sc.nextInt());
+                mother3.setBirthDate(sc.nextLine());
+                mother3.setSchedule(new HashMap<>());
+                System.out.print("mother task: ");
+                mother3.getSchedule().put(DayOfWeek.Wednesday, sc.nextLine());
+
+                fs.createNewFamily(father3, mother3);
+                Family family3 = fs.getAllFamilies().get(2);
+                family3.setFather(father3);
+                family3.setMother(mother3);
+                family3.setChildren(new ArrayList<>());
+                family3.setPet(new HashSet<>());
+
+                System.out.println(family3.prettyFormat());
+            } else if (navBar.equals("5")) {
+
+                System.out.println("delete family by its position: ");
+                int deleted = sc.nextInt();
+                System.out.print("Delete family in position: ");
+                fs.deleteFamilyByIndex(deleted);
+
+                //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            } else if (navBar.equals("6")) {
+                int position = sc.nextInt();
+                Family fam = fs.getFamilyById(position);
+                Iterator<Human> childs = fam.getChildren().iterator();
+                Human h = childs.next();
+                if (childs.hasNext()) {
+                    h.setBirthDate("24/11/1998");
+                }
             }
-        } catch (Exception ex) {
-            System.err.println("No familes found");
-        }
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        try {
-            System.out.print("Enter a number to get families having number of members less than that number: ");
-            Collection<Family> f2 = fs.getFamiliesLessThan(sc.nextInt());
-            Iterator<Family> iter2 = f2.iterator();
-            for(int i=1; iter2.hasNext(); i++) {
-                System.out.println(i + " Family:    " + iter2.next());
+            else if (navBar.equals("7")) {
+                Human adopted = new Human();
+                adopted.setName("adopted");
+                adopted.setSurname("adopted");
+                adopted.setIq(60);
+                adopted.setSchedule(new HashMap<>());
+                adopted.getSchedule().put(DayOfWeek.Sunday, tasks[0]);
+                fs.bornChild(family, adopted, gender_boy);
+                int limit = sc.nextInt();
+                fs.adoptChild(family, adopted);
+                int cnt2 = family.getChildren().size() + 2;
+                if (cnt2 > limit) throw new FamilyOverflowException("Error");
+            } else if (navBar.toLowerCase().equals("exit")) {
+                forAllMethods = false;
+            } else {
+                forAllMethods = true;
             }
-        } catch (Exception ex) {
-            System.err.println("No familes found");
         }
-
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        System.out.print("Enter a number to get families having number of members equals that number: ");
-        int member_cnt = sc.nextInt();
-        List<Family> fmm = fs.getAllFamilies().stream()
-                .filter(s->s.getChildren().size()+2==member_cnt)
-                .collect(Collectors.toList());
-        Iterator<Family> iter3 = fmm.iterator();
-        for(int i=1; iter3.hasNext(); i++) {
-            System.out.println(i + " Family:    " + iter3.next());
-        }
-
-
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        Human father3 = new Human();
-        System.out.print("father name: ");
-        father3.setName(sc.nextLine());
-        System.out.print("father surname: ");
-        father3.setSurname(sc.nextLine());
-        System.out.print("father IQ: ");
-        father3.setIq(sc.nextInt());
-        father3.setBirthDate(sc.nextLine());
-        father3.setSchedule(new HashMap<>());
-        System.out.print("father task: ");
-        father3.getSchedule().put(DayOfWeek.Thursday, sc.nextLine());
-
-        Human mother3 = new Human();
-        System.out.print("mother name: ");
-        mother3.setName(sc.nextLine());
-        System.out.print("mother surname: ");
-        mother3.setSurname(sc.nextLine());
-        System.out.print("mother IQ: ");
-        mother3.setIq(sc.nextInt());
-        mother3.setBirthDate(sc.nextLine());
-        mother3.setSchedule(new HashMap<>());
-        System.out.print("mother task: ");
-        mother3.getSchedule().put(DayOfWeek.Wednesday, sc.nextLine());
-
-        fs.createNewFamily(father3, mother3);
-        Family family3 = fs.getAllFamilies().get(2);
-        family3.setFather(father3);
-        family3.setMother(mother3);
-        family3.setChildren(new ArrayList<>());
-        family3.setPet(new HashSet<>());
-
-        System.out.println(family3.prettyFormat());
-
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        System.out.println("delete family by its position: ");
-        int deleted = sc.nextInt();
-        System.out.print("Delete family in position: ");
-        fs.deleteFamilyByIndex(deleted);
-
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        int position = sc.nextInt();
-        Family fam = fs.getFamilyById(position);
-        Iterator<Human> childs = fam.getChildren().iterator();
-        Human h = childs.next();
-        if (childs.hasNext()) {
-            h.setBirthDate("24/11/1998");
-        }
-
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-        //FamilyOverflowException foe = new FamilyOverflowException("Error");
-
-        Human adopted = new Human();
-        adopted.setName("adopted");
-        adopted.setSurname("adopted");
-        adopted.setIq(60);
-        adopted.setSchedule(new HashMap<>());
-        adopted.getSchedule().put(DayOfWeek.Sunday, tasks[0]);
-        fs.bornChild(family, adopted, gender_boy);
-            int limit = sc.nextInt();
-            fs.adoptChild(family, adopted);
-            int cnt2 = family.getChildren().size() + 2;
-            if (cnt2 > limit) throw new FamilyOverflowException("Error");
     }
 }
